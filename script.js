0c9a29eed2129b53f78ffd532501b436ae7a253d
Edward Wolcher <edwardwolcher@gmail.com>
2021-12-08 15:02:14 -0800

    minor color picking fix

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ function windowResized() {
 }
 
 function draw() {
-  if (!face || !drawFunction || !sentiment || !classifier) return;
+  if (!face.ready || !drawFunction || !sentiment || !classifier) return;
 
   if (!running) return;
   drawFunction.draw();
